20dc440e59ec6f23d4701bf613fd36ac5302212a
reversed_0 <arhum.ahmed.khan0@gmail.com>
2025-09-16 10:18:43 -0400

    Updated Lab_1

--- Labs/Lab_1/src/Part_A.java
+++ Labs/Lab_1/src/Part_A.java
@@ -1,4 +1,31 @@
 public class Part_A
 {
 
+    Part_A()
+    {
+        System.out.println(fibonacci(8));
+    }
+
+    int fibonacci(int n)
+    {
+        int fib1 = 0;
+        int fib2 = 1;
+
+        int fib = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            fib = fib1 + fib2;
+
+            fib2 = fib1;
+            fib1 = fib;
+        }
+
+        return fib;
+    }
+
+    public static void main(String[] args)
+    {
+        new Part_A();
+    }
 }
