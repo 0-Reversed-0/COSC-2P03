cf33c463860bd118aa1be3b53cdfe375e4735063
reversed_0 <arhum.ahmed.khan0@gmail.com>
2025-09-17 18:28:58 -0400

    Updated Lecture_2

--- Lectures/Lecture_2/src/InfixPostfix.java
+++ Lectures/Lecture_2/src/InfixPostfix.java
@@ -1,4 +1,22 @@
+import java.util.*;
+
 public class InfixPostfix
 {
 
+    /**
+     * In lecture, We went over converting Infix notation to Postfix using stacks.
+     * An infix expression has the notation of a normal expression, Example: a + b.
+     * A postfix expression has the notation of having the operands (variables/constants) come before the operator (+ - / *). Example: ab+
+     * 
+     */
+
+    public InfixPostfix()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        new InfixPostfix();
+    }
 }
