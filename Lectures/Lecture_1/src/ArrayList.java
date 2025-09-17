551370f89ea2dcf07f59ba05ac92b523b63e3bc6
reversed_0 <arhum.ahmed.khan0@gmail.com>
2025-09-17 01:37:17 -0400

    Updated Lab 1

--- Lectures/Lecture_1/src/ArrayList.java
+++ Lectures/Lecture_1/src/ArrayList.java
@@ -4,5 +4,6 @@ public class ArrayList
      * Note to self:
      * - Write basic implementation of an array list
      * - Make tester to see it work
+     * - Important to make sure the array keeps updating its space
      */
 }
